52749721811fd5eb99a38ae8f86e41961e820b10
ItsOnlyMe01 <134477804+ItsOnlyMe01@users.noreply.github.com>
2025-12-30 16:40:28 +0530

    integtaed grok apiii

--- Backend/Routes/transalteRoutes.js
+++ Backend/Routes/transalteRoutes.js
@@ -29,7 +29,7 @@ async function autoDetectAndTranslate(text, targetLanguage) {
     const response = await axios.post(
       "https://api.groq.com/openai/v1/chat/completions",
       {
-        model: "llama3-8b-8192",
+        model: "llama-3.1-8b-instant",
         messages: [
           {
             role: "system",
